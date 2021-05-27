8a8032424fb70a133e4f9d0b0abf9c061f6a1085
Yago Mouro <yagormouro@gmail.com>
2021-05-26 22:00:18 -0300

    fix formValidation file

--- js/formValidation.js
+++ js/formValidation.js
@@ -1,9 +1,11 @@
-var autor = document.getElementById('autor');
-var fonte = document.getElementById('fonte');
-var titulo = document.getElementById('titulo');
-var conteudo = document.getElementById('conteudo');
-var btn = document.getElementById('btnEnviar');
+const autor = document.getElementById('autor');
+const fonte = document.getElementById('fonte');
+const titulo = document.getElementById('titulo');
+const conteudo = document.getElementById('conteudo');
+const btn = document.getElementById('btnEnviar');
+const form = document.getElementById('form');
 
-btn.addEventListener('click', () => {
-  alert('')
+
+btn.addEventListener('click', (event) => {
+  event.preventDefault();
 })
