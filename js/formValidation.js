ee2b0bea0639c8ef157cb34bb318bba6f844fd61
Yago Mouro <yagormouro@gmail.com>
2021-05-26 21:39:34 -0300

    add formValidation file

---
+++ js/formValidation.js
@@ -0,0 +1,8 @@
+var autor = document.getElementById('autor');
+var fonte = document.getElementById('fonte');
+var titulo = document.getElementById('titulo');
+var conteudo = document.getElementById('conteudo');
+
+autor.addEventListener('click', () => {
+  alert('');
+})
